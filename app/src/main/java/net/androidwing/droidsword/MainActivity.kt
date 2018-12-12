3d9d706b89eb8c8f1e5a6ba24b9a86bbc4a9b677
wing <androidwing1992@gmail.com>
2018-12-12 22:01:18 +0800

    update ui

--- app/src/main/java/net/androidwing/droidsword/MainActivity.kt
+++ app/src/main/java/net/androidwing/droidsword/MainActivity.kt
@@ -3,11 +3,10 @@ package net.androidwing.droidsword
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Switch
-import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -15,13 +14,12 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    val button = findViewById(R.id.btn_startInstaller)
+
     if (isHook()) {
-      (findViewById(R.id.tv_is_hook) as TextView).text = "插件已激活,请放心使用"
-      button?.visibility = View.GONE
+      findViewById(R.id.ll_not_active)?.visibility = View.GONE
     }
 
-    button?.setOnClickListener {
+    findViewById(R.id.tv_start_xi)?.setOnClickListener {
       val intent = Intent()
       val componentName = ComponentName("de.robv.android.xposed.installer",
           "de.robv.android.xposed.installer.WelcomeActivity")
@@ -31,7 +29,7 @@ class MainActivity : AppCompatActivity() {
       try {
         startActivity(intent)
       } catch (e: Exception) {
-        Toast.makeText(MainActivity@ this, "啊哦，你好像没有安装XposedInstaller", Toast.LENGTH_SHORT).show()
+        Toast.makeText(MainActivity@ this, getString(R.string.no_xposed_installer), Toast.LENGTH_SHORT).show()
       }
     }
 
